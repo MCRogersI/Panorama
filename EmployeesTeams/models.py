06564d2232dff74c8a467a7da563608a1352a1e1
Manuel Rogers <mcrogers@uc.cl>
2017-02-01 17:22:51 -0300

    bases

--- EmployeesTeams/models.py
+++ EmployeesTeams/models.py
@@ -1,7 +1,7 @@
 from pony.orm import *
 
 db = Database()
-db.bind('postgres', user='postgres', password='hxq54ght3', host='localhost', database='panorama')
+db.bind('postgres', user='postgres', password='panorama', host='localhost', database='panorama')
 #se definen las clases (tablas en las bases de datos)
 
 class Employees(db.Entity):
