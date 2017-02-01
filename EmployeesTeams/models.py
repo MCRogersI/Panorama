eb91850665643c82a6e400e52c75f7476a7f09a8
Manuel Rogers <mcrogers@uc.cl>
2017-02-01 16:33:01 -0300

    definiciones de las tablas

---
+++ EmployeesTeams/models.py
@@ -0,0 +1,33 @@
+from pony.orm import *
+
+db = Database()
+db.bind('postgres', user='postgres', password='hxq54ght3', host='localhost', database='panorama')
+#se definen las clases (tablas en las bases de datos)
+
+class Employees(db.Entity):
+	id = PrimaryKey(int, auto=False)
+	name = Required(str)
+	team_id = Optional('Teams')
+	skills = Set('Skills')
+
+class Skills (db.Entity):
+	id = PrimaryKey(int, auto=False)
+	name = Required(str)
+	employees = Set(Employees)
+	team_skill = Set('Teams_Skills')
+
+
+class Teams(db.Entity):
+	id = PrimaryKey(int, auto=False)
+	zone = Required(int)
+	team_skills = Set('Teams_Skills')
+	Employees = Set(Employees)
+	
+
+class  Teams_Skills(db.Entity):
+	team = Required(Teams)
+	skill = Required(Skills)
+	PrimaryKey(team, skill)
+	performance = Required(float)
+
+db.generate_mapping(create_tables=True)
