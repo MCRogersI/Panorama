6eeec6adc10c2811cbcf56a100357609e43b518c
unknown <lacroquevielle@uc.cl>
2017-02-01 17:27:31 -0300

    Cambie el orden de "fab" y "inst" Please enter the commit message for your changes. Lines starting

--- EmployeesTeams/features.py
+++ EmployeesTeams/features.py
@@ -1,14 +1,14 @@
 from pony.orm import *
 from models import Employees, Teams, Skills, Teams_Skills
 
-def CreateEmpoyee(id,name,team_id,rect,des,inst,fab):
+def CreateEmpoyee(id, name, team_id, rect, des, fab, inst):
 	with db_session:
-		e=Employees(id = id, name = name, team_id = team_id)
+		e = Employees(id = id, name = name, team_id = team_id)
 		if rect ==1:
 			e.Skills.add(Skills[1])
 		if des ==1:
 			e.Skills.add(Skills[2])
-		if inst ==1:
-			e.Skills.add(Skills[3])
 		if fab ==1:
-			e.Skills.add(Skills[4])			
+			e.Skills.add(Skills[3])
+		if inst ==1:
+			e.Skills.add(Skills[4])
