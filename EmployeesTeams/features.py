f3a54968075724313f94a81a6fb7ec4fccb76c3a
unknown <lacroquevielle@uc.cl>
2017-02-01 17:39:15 -0300

    cambios chicos de espacio

--- EmployeesTeams/features.py
+++ EmployeesTeams/features.py
@@ -4,11 +4,11 @@ from models import Employees, Teams, Skills, Teams_Skills
 def CreateEmpoyee(id, name, team_id, rect, des, fab, inst):
 	with db_session:
 		e = Employees(id = id, name = name, team_id = team_id)
-		if rect ==1:
+		if rect == 1:
 			e.Skills.add(Skills[1])
-		if des ==1:
+		if des == 1:
 			e.Skills.add(Skills[2])
-		if fab ==1:
+		if fab == 1:
 			e.Skills.add(Skills[3])
-		if inst ==1:
+		if inst == 1:
 			e.Skills.add(Skills[4])
