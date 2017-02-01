7aa677e4f0809c1e28cc3a9a8bda5254d72d92b3
Manuel Rogers <mcrogers@uc.cl>
2017-02-01 17:20:18 -0300

    crear

---
+++ EmployeesTeams/features.py
@@ -0,0 +1,15 @@
+from pony.orm import *
+from models import Employees, Teams, Skills, Teams_Skills
+
+def CreateEmpoyee(id,name,team_id,rect,des,inst,fab):
+	with db_session:
+		e=Employees(id = id, name = name, team_id = team_id)
+		if rect ==1:
+			e.Skills.add(Skills[1])
+		if des ==1:
+			e.Skills.add(Skills[2])
+		if inst ==1:
+			e.Skills.add(Skills[3])
+		if fab ==1:
+			e.Skills.add(Skills[4])			
+		
