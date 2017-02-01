013920ad19387c365ab9c17e98b437b768e480d5
unknown <lacroquevielle@uc.cl>
2017-02-01 16:40:49 -0300

    Clave de la base de datos en models.py cambiada a "panorama"

--- EmployeesTeams/models.py
+++ EmployeesTeams/models.py
@@ -1,7 +1,7 @@
 from pony.orm import *
 
 db = Database()
-db.bind('postgres', user='postgres', password='hxq54ght3', host='localhost', database='panorama')
+db.bind('postgres', user='postgres', password='panorama', host='localhost', database='panorama')
 #se definen las clases (tablas en las bases de datos)
 
 class Employees(db.Entity):
@@ -22,7 +22,7 @@ class Teams(db.Entity):
 	zone = Required(int)
 	team_skills = Set('Teams_Skills')
 	Employees = Set(Employees)
-	
+
 
 class  Teams_Skills(db.Entity):
 	team = Required(Teams)
