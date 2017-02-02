48192db6eede22d26682605ebfd757d56a467188
Manuel Rogers <mcrogers@uc.cl>
2017-02-02 17:19:23 -0300

    agregue mapping

--- Projects/models.py
+++ Projects/models.py
@@ -3,3 +3,8 @@ from pony.orm import *
 db = Database()
 db.bind('postgres', user='postgres', password='panorama', host='localhost', database='panorama')
 #se definen las clases (tablas en las bases de datos)
+
+
+
+
+db.generate_mapping(create_tables=True)
