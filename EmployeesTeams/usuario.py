e0fd8e0e8817aa5a0d2dbc44f03853f90f32b4f0
Manuel Rogers <mcrogers@uc.cl>
2017-02-02 19:27:58 -0300

    agregué tasks_teams en teams

--- EmployeesTeams/usuario.py
+++ EmployeesTeams/usuario.py
@@ -1,7 +1,6 @@
 from features import *
 
 while True:
-<<<<<<< HEAD
     opt=input("Marque:\n 1 si desea crear empleados/equipos \n 2 si desea editar empleados/equipos \n 3 para ver empleados/equipos actuales \n 4 para salir \n")
     if(opt=='1'):
         opt1=input("Marque \n 1 si desea crear empleado \n 2 si desea crear un equipo\n")
@@ -108,30 +107,3 @@ while True:
     if(opt == '4'):
         break
 
-
-=======
-	opt=input("\n Marque una de las siguientes opciones:\n - 1: si desea crear empleados/equipos. \n - 2: si desea editar empleados/equipos. \n - 3: para ver empleados/equipos actuales \n - 4: para salir. \n Ingrese la alternativa elegida: ")
-	if(opt=='1'):
-		opt1=input("Marque \n 1 si desea crear empleado \n 2 si desea crear un equipo\n")
-		if(opt1=='1'):
-	#el usuario debe entregar siempre un string con id,nombre,teamid,True,False,True,True (ejemplo)
-			emp=input("Ingrese las características del empleado: ")
-			CreateEmployee(emp.split(",")[0],emp.split(",")[1],emp.split(",")[2],emp.split(",")[3],emp.split(",")[4],emp.split(",")[5],emp.split(",")[6])
-			#print(emp.split(",")[0],emp.split(",")[1],emp.split(",")[2],emp.split(",")[3])
-
-    if(opt=='2'):
-	#de esta forma deberia entregar siempre todas las características del usuario
-		opt2=input("Marque \n 1 si desea editar empleado \n 2 si desea editar un equipo")
-		if(opt2=='1'):
-			emp2=input("Ingrese las características nuevas del empleado")
-
-	if(opt=='3'):
-		opt3=input("Marque \n 1 si desea ver empleados \n 2 si desea ver equipos")
-		if(opt3=='1'):
-			PrintEmployees()
-		if(opt3=='2'):
-			PrintTeams()
-
-	if(opt == '4'):
-		break
->>>>>>> a4915c62d183476be47d55cd4eb434e2eaf28c24
