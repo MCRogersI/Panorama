3184088cd25296d111a2eedbce83cb0b73717300
MauricioT123 <mjtroncoso@uc.cl>
2017-02-02 13:29:45 -0300

    comentarios a usuario.py

--- EmployeesTeams/console.py
+++ EmployeesTeams/console.py
@@ -1,8 +1,8 @@
 from features import *
 
-#CreateEmployee(2,'Roberto',1,True,True,True,False)
-#EditEmployee(2,new_name = 'Ricardo',rect = False)
 #CreateTeam(1, 1, perf_rect=4)
+#CreateEmployee(2,'Roberto',1,True,True,True,False)
+EditEmployee(2,new_name = 'Ricardo',rect = False)
 PrintTeamsSkills()
 PrintEmployees()
 PrintTeams()
--- EmployeesTeams/usuario.py
+++ EmployeesTeams/usuario.py
@@ -5,10 +5,12 @@ opt=input("Marque:\n 1 si desea crear empleados/equipos \n 2 si desea editar emp
 if(opt=='1'):
     opt1=input("Marque \n 1 si desea crear empleado \n 2 si desea crear un equipo\n")
     if(opt1=='1'):
+#el usuario debe entregar siempre un string con id,nombre,teamid,True,False,True,True (ejemplo)
         emp=input("Ingrese las características del empleado: ")
         CreateEmployee(emp.split(",")[0],emp.split(",")[1],emp.split(",")[2],emp.split(",")[3],emp.split(",")[4],emp.split(",")[5],emp.split(",")[6])
         #print(emp.split(",")[0],emp.split(",")[1],emp.split(",")[2],emp.split(",")[3])
 if(opt=='2'):
+#de esta forma deberia entregar siempre todas las características del usuario
     opt2=input("Marque \n 1 si desea editar empleado \n 2 si desea editar un equipo")
     if(opt2=='1'):
         emp2=input("Ingrese las características nuevas del empleado")
