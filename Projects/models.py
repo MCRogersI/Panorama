32d43f9664bc51fbfa5a6a4bd81f08c4393124fd
Manuel Rogers <mcrogers@uc.cl>
2017-02-02 18:19:42 -0300

    usuario merge

--- Projects/models.py
+++ Projects/models.py
@@ -1,10 +1,17 @@
 from pony.orm import *
+from datetime import datetime
 
 db = Database()
 db.bind('postgres', user='postgres', password='panorama', host='localhost', database='panorama')
 #se definen las clases (tablas en las bases de datos)
 
 
+class Tasks(db.Entity):
+	id = PrimaryKey(int, auto=False)
+	id_skill = Required(Skill)
+	id_proyect = Required('Proyect')
+	original_initial_date = Required(datetime)
+	
 
 
 db.generate_mapping(create_tables=True)
