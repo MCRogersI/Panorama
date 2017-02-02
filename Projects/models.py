0d6d4eb263757c13c58b7261ddce8b40f70fd043
unknown <lacroquevielle@uc.cl>
2017-02-02 17:15:11 -0300

    models.py creado en carpeta Projects

---
+++ Projects/models.py
@@ -0,0 +1,5 @@
+from pony.orm import *
+
+db = Database()
+db.bind('postgres', user='postgres', password='panorama', host='localhost', database='panorama')
+#se definen las clases (tablas en las bases de datos)
