a5bae66065b1599c53d523bfb6235d81682f0371
Manuel Rogers <mcrogers@uc.cl>
2017-02-03 16:37:58 -0300

    agregar edicion task

--- Projects/features.py
+++ Projects/features.py
@@ -31,10 +31,32 @@ def EditProject(db, contract_number, new_client_address = None, new_client_name 
 			p.real_cost = new_real_cost	
 
 
-def CreateTask(db, id , id_skill, id_project, original_initial_date, original_end_date, efective_initial_date = None, efective_end_date = None, failed = None, fail_cost = None):
+def CreateTask(db, id, id_skill, id_project, original_initial_date, original_end_date, efective_initial_date = None, efective_end_date = None, failed = None, fail_cost = None):
 	with db_session:
 		t = db.Tasks(id = id, id_skill = id_skill, id_project = id_project, original_initial_date = original_initial_date, original_end_date = original_end_date)
 
+def EditTask(db, id , id_skill = None, id_project = None, original_initial_date = None, original_end_date = None, efective_initial_date = None, efective_end_date = None, failed = None, fail_cost = None):
+	with db_session:
+		t = db.Tasks[id]
+		if id_skill != None:
+			t.id_skill = id_skill
+		if id_project != None:
+			t.id_project = id_project
+		if original_initial_date != None:
+			t.original_initial_date = original_initial_date
+		if original_end_date != None:
+			t.original_end_date = original_end_date
+		if efective_initial_date != None:
+			t.efective_initial_date = efective_initial_date
+		if efective_end_date != None:
+			t.efective_end_date = efective_end_date
+		if failed != None:
+			t.failed = failed
+		if fail_cost != None:
+			t.fail_cost = fail_cost
+			
+# def DeleteTask(db, id_task):
+	
 
 def PrintTasks(db):
 	with db_session:
