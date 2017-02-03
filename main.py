9243e3c2f65b806016a9dbede710869bcc1f0107
unknown <lacroquevielle@uc.cl>
2017-02-03 13:59:45 -0300

    nada especial

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ from pony.orm import *
 from database import db
 import EmployeesTeams.features as ETf, EmployeesTeams.usuario as ETu
 
-#ETu.employees_teams_console(db, ETf.CreateEmployee, ETf.PrintEmployees, ETf.EditEmployee, ETf.CreateTeam, ETf.PrintTeams, ETf.PrintTeamsSkills, ETf.EditTeam, ETf.PrintSkills)
+ETu.employees_teams_console(db, ETf.CreateEmployee, ETf.PrintEmployees, ETf.EditEmployee, ETf.CreateTeam, ETf.PrintTeams, ETf.PrintTeamsSkills, ETf.EditTeam, ETf.PrintSkills)
