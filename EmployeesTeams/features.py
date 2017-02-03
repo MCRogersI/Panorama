4171cd7917baa94880e599fc5c0e197cb27efc99
unknown <lacroquevielle@uc.cl>
2017-02-03 13:26:58 -0300

    arregle EditTeam en features.py

--- EmployeesTeams/features.py
+++ EmployeesTeams/features.py
@@ -66,13 +66,13 @@ def EditTeam(db, id, new_zone = None, perf_rect = None, perf_des = None, perf_fa
 		if new_zone != None:
 			t.zone = new_zone
 		if perf_rect != None:
-			db.Teams_Skills(team = id, skill = 1, performance = perf_rect)
+			db.Teams_Skills[(id, 1)].performance = perf_rect
 		if perf_des != None:
-			db.Teams_Skills(team = id, skill = 2, performance = perf_des)
+			db.Teams_Skills[(id, 2)].performance = perf_des
 		if perf_fab != None:
-			db.Teams_Skills(team = id, skill = 3, performance = perf_fab)
+			db.Teams_Skills[(id, 3)].performance = perf_fab
 		if perf_inst != None:
-			db.Teams_Skills(team = id, skill = 4, performance = perf_inst)
+			db.Teams_Skills[(id, 4)].performance = perf_inst
 
 def PrintSkills(db):
 	with db_session:
--- main.py
+++ main.py
@@ -3,4 +3,3 @@ from database import db
 import EmployeesTeams.features as ETf, EmployeesTeams.usuario as ETu
 
 ETu.employees_teams_console(db, ETf.CreateEmployee, ETf.PrintEmployees, ETf.EditEmployee, ETf.CreateTeam, ETf.PrintTeams, ETf.PrintTeamsSkills, ETf.EditTeam, ETf.PrintSkills)
-
