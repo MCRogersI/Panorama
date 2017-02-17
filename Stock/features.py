aaebc461666fe5210bac33c85c20a74964cc1286
MauricioT123 <mjtroncoso@uc.cl>
2017-02-17 14:52:43 -0300

    arreglar conflictos

--- Stock/features.py
+++ Stock/features.py
@@ -1,9 +1,13 @@
 from pony.orm import *
 
 def createSKU(db, name, price, critical_level, real_quantity = None, estimated_quantity = None):
+	''' Este método crea una unidad nueva de stock, asigna automáticamente el ID de la misma.
+		La cantidad estimada es la que se ve afectada por una planificación que podría cambiarse 
+		en el futuro '''
 	with db_session:
 		s = db.SKU(name = name, price = price, critical_level = critical_level, real_quantity = real_quantity, estimated_quantity = estimated_quantity)
 def editSKU(db, id, name = None, price = None, critical_level = None, real_quantity = None, estimated_quantity = None):
+	''' Este método edita la unidad de stock, en cualquiera de sus características '''
 	with db_session:
 		s = db.SKU[id]
 		if name != None:
