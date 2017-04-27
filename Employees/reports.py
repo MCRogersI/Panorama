075113f26be36777ab51d8a8309139ccda8c6c5d
Gonzalo-Perez <gaperez2@uc.cl>
2017-04-26 22:31:47 -0400

    Avance reports

--- Employees/reports.py
+++ Employees/reports.py
@@ -5,6 +5,8 @@ import numpy as np
 from openpyxl import Workbook, load_workbook
 from openpyxl.styles.borders import Border, Side
 from openpyxl.styles import Font,Alignment
+from openpyxl.styles import PatternFill
+from openpyxl.styles.colors import Color
 import os
 
 def createEmployeeReportV1(db,id_employee):
@@ -178,55 +180,70 @@ from database import db
 
 def createEmployeeReportV2(db,id_employee):
     ''' Este método crea un informe en Excel compacto con una proción de la información de la base de datos. '''
-    wb = Workbook()
-    by_default_sheet = wb.get_sheet_by_name('Sheet')
-    by_default_sheet.title = 'Introducción del informe'
-    wb.remove_sheet(by_default_sheet)
-    ws = wb.create_sheet(
-        "Informe trabajador",index=0)
-
-    widths = {"A": 20, "B": 20, "C": 20,"D": 20, "E": 20, "F": 20,
-              "G": 20, "H": 20, "I": 20,"J": 20, "K": 20, "L": 20,
-              "M": 20, "N": 20}
-
-    heights = {"A": 10, "B": 10, "C": 10, "D": 10, "E": 10, "F": 10,
-              "G": 10, "H": 10, "I": 10, "J": 10, "K": 10, "L": 10,
-              "M": 10, "N": 10}
-
-    thin_border = Border(left=Side(style='thin'),
-                         right=Side(style='thin'),
-                         top=Side(style='thin'),
-                         bottom=Side(style='thin'))
-
-
-    columns = ["A", "B", "C","D","E","F","G","H","I"]
-    for c in columns:
-        ws.column_dimensions[c].width = widths[c]
-        ws.column_dimensions[c].height = heights[c]
-
-
-    num_columns, letter_columns = zip(*list(enumerate(columns)))
-    num_columns = list(num_columns)
-    num_columns = [x + 1 for x in num_columns] #Desplazamos los valores en 1 porque Excel está indexado desde el 1 y no desde el 0
-    letter_columns = list(letter_columns)
-
-    # escribir los títulos de las columnas, en negrita
-
-    texts = ["","","","ID", "NOMBRE", "ZONA", "COMPETENCIA",
-             "SENIOR","RENDIMIENTO"]
-
-    for i in range(4,len(num_columns)+1):
-        cell = ws.cell(row=i, column=3, value=texts[i-1])
-        cell.font = Font(bold=True,)
-        cell.border = thin_border
-        cell.alignment = Alignment(horizontal='left')
-
-
-    # A continuación llenamos con los datos
-
-
     with db_session:
         e = db.Employees.get(id=id_employee)
+        es = db.Employees_Skills.get(employee=e)
+        # ea = select(ea for ea in db.Employees_Activities if ea.employee == e)
+        ea = db.Employees_Activities.get(employee=e)
+        # print(ea.id) #Revisar creación de activities en consola
+        wb = Workbook()
+        by_default_sheet = wb.get_sheet_by_name('Sheet')
+        by_default_sheet.title = 'Introducción del informe'
+        wb.remove_sheet(by_default_sheet)
+        ws = wb.create_sheet(
+            "Informe trabajador",index=0)
+
+        widths = {"A": 20, "B": 20, "C": 20,"D": 20, "E": 20, "F": 20,
+                  "G": 20, "H": 20, "I": 20,"J": 20, "K": 20, "L": 20,
+                  "M": 20, "N": 20}
+
+        heights = {"A": 10, "B": 10, "C": 10, "D": 10, "E": 10, "F": 10,
+                  "G": 10, "H": 10, "I": 10, "J": 10, "K": 10, "L": 10,
+                  "M": 10, "N": 10}
+
+        thin_border = Border(left=Side(style='thin'),
+                             right=Side(style='thin'),
+                             top=Side(style='thin'),
+                             bottom=Side(style='thin'))
+
+        if (es.skill.id == 4):
+            # columns = ["A", "B", "C","D","E","F","G","H","I"] #Para un instalador y con el rendimiento
+            columns = ["A", "B", "C", "D", "E", "F", "G", "H"]  # Para un instalador sin el rendimiento
+            # texts = ["","","","ID", "NOMBRE", "ZONA", "COMPETENCIA",
+            #          "SENIOR","RENDIMIENTO"] #Para un instalador y con el rendimiento
+            texts = ["","","","ID", "NOMBRE", "ZONA", "COMPETENCIA",
+                     "SENIOR"] #Para un instalador sin el rendimiento
+        else:
+            columns = ["A", "B", "C", "D", "E", "F", "G"]
+            texts = ["", "", "", "ID", "NOMBRE", "ZONA", "COMPETENCIA"]
+
+        aux_columns = ["A", "B", "C", "D", "E", "F", "G", "H"] #Solo para mantener el ancho de las celdas en el caso que no sea un instalador
+
+        for c in aux_columns:
+            ws.column_dimensions[c].width = widths[c]
+            ws.column_dimensions[c].height = heights[c]
+
+
+        num_columns, letter_columns = zip(*list(enumerate(columns)))
+        num_columns = list(num_columns)
+        num_columns = [x + 1 for x in num_columns] #Desplazamos los valores en 1 porque Excel está indexado desde el 1 y no desde el 0
+        letter_columns = list(letter_columns)
+
+        # escribir los títulos de las columnas, en negrita
+
+
+
+
+        for i in range(4,len(num_columns)+1):
+            cell = ws.cell(row=i, column=3, value=texts[i-1])
+            cell.font = Font(bold=True,)
+            cell.border = thin_border
+            cell.alignment = Alignment(horizontal='left')
+
+
+        # A continuación llenamos con los datos
+
+
         r=4
         #Escribe el ID del empleado
         if e.id != None:
@@ -265,7 +282,7 @@ def createEmployeeReportV2(db,id_employee):
             cell.alignment = Alignment(horizontal='left')
 
         # Escribe la competencia del empleado (por el momento se asume que solo es una)
-        es = db.Employees_Skills.get(employee = e)
+
         if es.skill != None:
             skill_name = es.skill.name
             cell = ws.cell(row=r+3, column=4, value=skill_name)
@@ -281,7 +298,7 @@ def createEmployeeReportV2(db,id_employee):
         # Escribe si el empleado es senior o no (Sí o No)
         if(es.skill.id == 4):
             if e.senior != None:
-                if es.senior:
+                if e.senior:
                     value = "Sí"
                 else:
                     value = "No"
@@ -333,13 +350,15 @@ def createEmployeeReportV2(db,id_employee):
             cell.alignment = Alignment(horizontal='center')
             calendar_r+=1
 
-        for f in [i for i in range(14, 14+len(dates))]:
+        for f in [i for i in range(14, 14+len(dates))]:#Revisar los limites de indexación
             ws.row_dimensions[f].height = 40
             for c in [i for i in range(4,9)]:
                 cell = ws.cell(row=f, column=c)
                 cell.font = Font(bold=True)
                 cell.border = thin_border
-                cell.alignment = Alignment(horizontal='center')
+                # cell.alignment = Alignment(horizontal='center')
+                wrap_alignment = Alignment(wrap_text=True, horizontal="center",vertical="center")
+                cell.alignment = wrap_alignment
 
 
 
@@ -361,7 +380,8 @@ def createEmployeeReportV2(db,id_employee):
                 if(not isNotWorkday(task_initial_d + working_days_counter*timedelta(days=1))):
                     working_dates.append(task_initial_d + working_days_counter*timedelta(days=1))
                 else:
-                    working_days_span+=1
+                    # working_days_span+=1 #Ya está considerado en el Planning
+                    pass
                 working_days_counter+=1
 
 
@@ -381,7 +401,7 @@ def createEmployeeReportV2(db,id_employee):
                     if (dates[i] <= d < dates[i + 1]):
                         # cell = ws.cell(row=r +10 + i, column=4+task_initial_d.weekday(), value="{0} \n Proyecto: {1}".format(task_initial_d, task.project))
                         cell = ws.cell(row=r + 10 + i, column=4 + d.weekday(),
-                                       value="Proyecto {}".format(task.project))
+                                       value="Proyecto {0}\n{1}".format(task.project,task.project.client_address))
                         cell.font = Font(bold=True)
                         cell.border = thin_border
                         cell.alignment = Alignment(horizontal='center')
@@ -391,6 +411,64 @@ def createEmployeeReportV2(db,id_employee):
                                                    vertical="center")
                         # assign
                         cell.alignment = wrap_alignment
+                        cell.fill = PatternFill("solid", fgColor="ffff00")
+                    elif (dates[len(dates) - 1] <= d):
+                        # cell = ws.cell(row=r +10 + i, column=4+task_initial_d.weekday(), value="{0} \n Proyecto: {1}".format(task_initial_d, task.project))
+                        cell = ws.cell(row=r + 10 + i + 1, column=4 + d.weekday(),
+                                       value="Proyecto {0}\n{1}".format(task.project,
+                                                                        task.project.client_address))
+                        cell.font = Font(bold=True)
+                        cell.border = thin_border
+                        cell.alignment = Alignment(horizontal='center')
+                        # cell.style.alignment.wrap_text = True #Para autoajustar el tamaño de la celca. Revisar su correcto funcionamiento
+                        # create alignment style
+                        wrap_alignment = Alignment(wrap_text=True, horizontal="center",
+                                                   vertical="center")
+                        # assign
+                        cell.alignment = wrap_alignment
+                        cell.fill = PatternFill("solid", fgColor="ffff00")
+
+        # Imprimir los días feriados
+        all_the_days_on_display = []
+        for i in range(0,span_of_weeks*7):
+            all_the_days_on_display.append(initial_week_date+timedelta(days=i))
+        for i in range(0, len(dates)-1):
+            for d in all_the_days_on_display:
+                if (dates[i] <= d < dates[i + 1]):
+                    if(isHoliday(d) and d.weekday() < 5):
+                        cell = ws.cell(row=r + 10 + i, column=4 + d.weekday(),value="Feriado")
+                        cell.font = Font(bold=True)
+                        cell.border = thin_border
+                        cell.alignment = Alignment(horizontal='center')
+                        wrap_alignment = Alignment(wrap_text=True, horizontal="center",vertical="center")
+                        cell.alignment = wrap_alignment
+                        cell.fill = PatternFill("solid", fgColor="00ffff")
+                    elif(ws.cell(row=r + 10 + i, column=4 + d.weekday()).value==None and d.weekday() < 5):
+                        ws.cell(row=r + 10 + i, column=4 + d.weekday()).value = "Disponible"
+                        ws.cell(row=r + 10 + i, column=4 + d.weekday()).fill = PatternFill("solid", fgColor="00ff00")
+                elif(dates[len(dates)-1] <= d):
+                    if (isHoliday(d) and d.weekday() < 5):
+                        cell = ws.cell(row=r + 10 + i + 1, column=4 + d.weekday(), value="Feriado")
+                        cell.font = Font(bold=True)
+                        cell.border = thin_border
+                        cell.alignment = Alignment(horizontal='center')
+                        wrap_alignment = Alignment(wrap_text=True, horizontal="center",
+                                                   vertical="center")
+                        cell.alignment = wrap_alignment
+                        cell.fill = PatternFill("solid", fgColor="00ffff")
+                    elif (ws.cell(row=r + 10 + i + 1, column=4 + d.weekday()).value == None and d.weekday() < 5):
+                        ws.cell(row=r + 10 + i + 1, column=4 + d.weekday()).value = "Disponible"
+                        ws.cell(row=r + 10 + i + 1, column=4 + d.weekday()).fill = PatternFill("solid", fgColor="00ff00")
+
+
+        # for f in [i for i in range(14, 14+len(dates))]:
+        #     ws.row_dimensions[f].height = 40
+        #     for c in [i for i in range(4,9)]:
+        #         cell = ws.cell(row=f, column=c)
+        #         cell.font = Font(bold=True)
+        #         cell.border = thin_border
+        #         cell.alignment = Alignment(horizontal='center')
+
 
 
 
@@ -408,4 +486,16 @@ def createEmployeeReportV2(db,id_employee):
     fn = os.path.join(report_folder_path,report_file_name)
     wb.save(fn)
 
+createEmployeeReportV2(db,1)
+createEmployeeReportV2(db,2)
 createEmployeeReportV2(db,3)
+createEmployeeReportV2(db,4)
+createEmployeeReportV2(db,5)
+createEmployeeReportV2(db,6)
+createEmployeeReportV2(db,7)
+createEmployeeReportV2(db,8)
+createEmployeeReportV2(db,9)
+createEmployeeReportV2(db,10)
+createEmployeeReportV2(db,11)
+createEmployeeReportV2(db,12)
+createEmployeeReportV2(db,13)
--- Stock/reports.py
+++ Stock/reports.py
@@ -5,6 +5,8 @@ import numpy as np
 from openpyxl import Workbook, load_workbook, drawing
 from openpyxl.styles import Font
 import openpyxl
+from openpyxl import Workbook
+from openpyxl.chart import BarChart, Series, Reference
 import os
 
 def baseCreateReport():
@@ -43,4 +45,87 @@ def baseCreateReport():
     fn = os.path.join(report_folder_path, report_file_name)
     wb.save(fn)
 
-# baseCreateReport()
+
+
+def createStockReport(db):
+    id_sku = 1
+    from Stock.features import displayStock, calculateStockForExcel
+    displayStock(db,id_sku)
+    base = calculateStockForExcel(db,id_sku)
+    dates = base[0]
+    values = base[1]
+
+    wb = Workbook(write_only=True)
+    ws = wb.create_sheet()
+
+    # rows = [
+    #     ('Fechas', 'Cantidad', 'Batch 2'),
+    #     (2, 10, 30),
+    #     (3, 40, 60),
+    #     (4, 50, 70),
+    #     (5, 20, 10),
+    #     (6, 10, 40),
+    #     (7, 50, 30),
+    # ]
+    # vals = [(i,np.sqrt(i)) for i in range(0,20)]
+    vals = [(i, np.sqrt(i)) for i in range(0, 20)]
+    rows = [('Fechas', 'Cantidad')]
+    rows.extend(vals)
+
+    for row in rows:
+        ws.append(row)
+
+    chart1 = BarChart()
+    chart1.type = "col"
+    chart1.style = 10
+    chart1.title = "Status del SKU"
+    chart1.y_axis.title = 'Cantidad'
+    chart1.x_axis.title = 'Fecha'
+
+    # data = Reference(ws, min_col=2, min_row=1, max_row=7, max_col=3)
+    data = Reference(ws, min_col=2, min_row=1, max_row=21)
+    cats = Reference(ws, min_col=1, min_row=2, max_row=21)
+    chart1.add_data(data, titles_from_data=True)
+    chart1.set_categories(cats)
+    chart1.shape = 4
+    ws.add_chart(chart1, "A10")
+
+    # from copy import deepcopy
+    #
+    # chart2 = deepcopy(chart1)
+    # chart2.style = 11
+    # chart2.type = "bar"
+    # chart2.title = "Horizontal Bar Chart"
+    #
+    # ws.add_chart(chart2, "G10")
+    #
+    # chart3 = deepcopy(chart1)
+    # chart3.type = "col"
+    # chart3.style = 12
+    # chart3.grouping = "stacked"
+    # chart3.overlap = 100
+    # chart3.title = 'Stacked Chart'
+    #
+    # ws.add_chart(chart3, "A27")
+    #
+    # chart4 = deepcopy(chart1)
+    # chart4.type = "bar"
+    # chart4.style = 13
+    # chart4.grouping = "percentStacked"
+    # chart4.overlap = 100
+    # chart4.title = 'Percent Stacked Chart'
+    #
+    # ws.add_chart(chart4, "G27")
+
+    module_path = os.path.dirname(__file__)
+    panorama_folder_path = os.path.abspath(os.path.join(module_path, os.pardir))
+    report_folder_path = os.path.join(panorama_folder_path, "Reportes")
+    if not os.path.exists(report_folder_path):
+        os.makedirs(report_folder_path)
+    report_file_name = "Informe de Stock.xlsx"
+    fn = os.path.join(report_folder_path, report_file_name)
+    wb.save(fn)
+
+
+from database import db
+createStockReport(db)
